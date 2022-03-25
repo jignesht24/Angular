2a23c5e7f67d1736b88496775a1e9f2442cc5584
Jignesh Trivedi <jignesht24@gmail.com>
2022-03-25 19:29:23 +0530

    Add files via upload

---
+++ LazyLoading/src/app/feature-module-example/demo-component/demo-component.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-demo-component',
+  templateUrl: './demo-component.component.html',
+  styleUrls: ['./demo-component.component.sass']
+})
+export class DemoComponentComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
